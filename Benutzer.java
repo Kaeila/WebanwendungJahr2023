ad59c47911a9e35fa62a24042e7f60074a5a1769
ItaxIzu12 <140389763+ItaxIzu12@users.noreply.github.com>
2024-01-15 13:49:03 +0100

    Add files via upload

---
+++ Benutzer.java
@@ -0,0 +1,54 @@
+package de.hhbk.model;
+
+import de.hhbk.model.enums.PersonAnrede;
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+
+@NamedQueries
+({
+    @NamedQuery(name="findByEmailAndPassword", query="SELECT u FROM Benutzer u WHERE u.email = :email AND u.passwort = :passwort"),
+    @NamedQuery(name="findByUsernameAndPassword", query="SELECT b.benutzername, b.passwort FROM Benutzer b WHERE b.benutzername = :benutzername AND b.passwort = :passwort")
+})
+
+
+@Entity
+public class Benutzer extends Person
+{
+  //-------------------------------------------------------------------------
+  //  Var(s)
+  //-------------------------------------------------------------------------     
+    protected String benutzername;
+    protected String passwort;
+    protected boolean isAdmin;
+
+
+  //-------------------------------------------------------------------------
+  //  Constructor(s)
+  //-------------------------------------------------------------------------     
+    public Benutzer() { super(); }
+
+    public Benutzer(String vorname, String nachname, String email, String passwort) 
+    { 
+        this(PersonAnrede.EMPTY, vorname, nachname, email, passwort);  
+    } 
+
+    public Benutzer(PersonAnrede anrede, String vorname, String nachname, String email, String passwort) 
+    { 
+        super(vorname, nachname, email); 
+        this.anrede = anrede;
+        this.passwort = passwort;
+    } 
+
+
+  //-------------------------------------------------------------------------
+  //  Get / Set
+  //-------------------------------------------------------------------------     
+    public String getPasswort() { return passwort; }
+
+    public void setPasswort(String passwort) { this.passwort = passwort; }
+
+
+
+}
---
+++ BenutzerBean.java
@@ -0,0 +1,77 @@
+package de.hhbk.web.beans;
+
+import de.hhbk.dao.BenutzerDao;
+import de.hhbk.model.Benutzer;
+import de.hhbk.model.enums.PersonAnrede;
+import java.util.Arrays;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import javax.servlet.http.HttpSession;
+
+
+@Named(value = "benutzer") 
+@SessionScoped
+public class BenutzerBean extends BeanTemplate<Benutzer, BenutzerDao>
+{
+  //-------------------------------------------------------------------------
+  //  Constructor(s)
+  //-------------------------------------------------------------------------     
+    public BenutzerBean() { super(new BenutzerDao()); } 
+
+    @PostConstruct
+    public void init()
+    { 
+        resetItem(); 
+    }
+
+  //-------------------------------------------------------------------------
+  //  Login / Logout
+  //-------------------------------------------------------------------------     
+    private String benutzername;
+    private String passwort;
+
+    public String getBenutzername() { return benutzername; }
+
+    public void setBenutzername(String benutzername) { this.benutzername = benutzername; }
+
+    public String getPasswort() { return passwort; }
+
+    public void setPasswort(String passwort) { this.passwort = passwort; }
+           
+    public String doLogin() throws Exception 
+    {
+      BenutzerDao benutzerDao = new BenutzerDao();
+      if (benutzerDao.isValidCredentials(benutzername, passwort)) {
+        HttpSession websession = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true); 
+        websession.setAttribute("benutzername", benutzername);
+        websession.setAttribute("MyLoginObject", true);
+        return "backend/empty.xhtml?faces-redirect=true"; 
+      } 
+      else {
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Fehler", "Benutzername oder Passwort ist falsch");
+        FacesContext.getCurrentInstance().addMessage(null, message);
+        return "";
+      }
+     
+    }
+    
+    public String doLogout() 
+    {  
+        HttpSession websession = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true); 
+        websession.removeAttribute("MyLoginObject"); 
+        return "/login.xhtml?faces-redirect=true";
+    }  
+    
+    
+  //-------------------------------------------------------------------------
+  //  Get / Set
+  //-------------------------------------------------------------------------     
+    public List getAnredeOptionen() { return Arrays.asList(PersonAnrede.values()); }
+    
+ 
+    
+}
